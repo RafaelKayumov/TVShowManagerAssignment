e9134468433e1c4d5a85cd70239830cbb4ce2f7a
Rafael Kayumov <justpoc@gmail.com>
2020-05-29 16:49:47 +0300

    Initial

---
+++ TVShowManagerAssignment/Source/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TVShowManagerAssignment
+//
+//  Created by Rafael Kayumov on 29/05/2020.
+//  Copyright © 2020 Rafael Kayumov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
