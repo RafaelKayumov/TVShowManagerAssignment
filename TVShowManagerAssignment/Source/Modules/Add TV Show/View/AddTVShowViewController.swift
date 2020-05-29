a4fe84d3afd21e52f9176b27039d8e322b9375e7
Rafael Kayumov <justpoc@gmail.com>
2020-05-29 18:45:43 +0300

    Add basic implementation of AddTVShowViewController

---
+++ TVShowManagerAssignment/Source/Modules/Add TV Show/View/AddTVShowViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddTVShowViewController.swift
+//  TVShowManagerAssignment
+//
+//  Created by Rafael Kayumov on 29/05/2020.
+//  Copyright © 2020 Rafael Kayumov. All rights reserved.
+//
+
+import UIKit
+
+class AddTVShowViewController: UIViewController {
+
+    @IBOutlet private weak var titleTextField: UITextField!
+    @IBOutlet private weak var releaseYearTextField: UITextField!
+    @IBOutlet private weak var numberOfSeasonsTextField: UITextField!
+    @IBOutlet private weak var saveButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
